7e9d5e3df429e29fa983c323c24c9bfbfd180643
apano-on <albulen.pano@unibz.it>
2023-10-28 00:12:49 +0200

    Update

--- WP2 Thematic Enrichment/sudtirol/gdal/script.sh
+++ WP2 Thematic Enrichment/sudtirol/gdal/script.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 
 PG_CONN='"host=host.docker.internal user=citydb dbname=sudtirol_db password=citydb port=7777"'
-WFS_CART_URL=https://geoservices3.civis.bz.it/geoserver/gvcc-Cartography/ows?service=wfs&version=2.0.0&request=getCapabilities
-
-# Define a variable to keep track of the current retry count
-retry_count=0
+#WFS_CART_URL_1=https://geoservices1.civis.bz.it/geoserver/p_bz-AdministrativeUnits/ows?service=wfs&version=2.0.0&request=getCapabilities
+#WFS_CART_URL_3=https://geoservices3.civis.bz.it/geoserver/gvcc-Cartography/ows?service=wfs&version=2.0.0&request=getCapabilities
+WFS_CART_URL=https://geoservicesservice_no.civis.bz.it/geoserver/feature_name/ows?service=wfs
 
 # Define the maximum number of retries
 max_retries=3
 
 # Define list of layers
 table_layer_pair=(
-  "construction_01k gvcc-Cartography:ConstructionAreas-01k"
-  "roofarea_01k gvcc-Cartography:RoofAreasScale-01k"
+  "1 municipalities p_bz-AdministrativeUnits p_bz-AdministrativeUnits:Municipalities"
+  "3 construction_01k gvcc-Cartography gvcc-Cartography:ConstructionAreas-01k"
+  "3 roofarea_01k gvcc-Cartography gvcc-Cartography:RoofAreasScale-01k"
   )
 
 for pair in "${table_layer_pair[@]}"; do
   max_retries=3
   echo "Processing: $pair"
   IFS=' ' read -ra parts <<< "$pair"
-  table="${parts[0]}"
-  layer="${parts[1]}"
+  service_no="${parts[0]}"
+  table="${parts[1]}"
+  feature="${parts[2]}"
+  layer="${parts[3]}"
+
+  # Replace geoservice number
+  WFS_CART_URL_t1=${WFS_CART_URL/service_no/$service_no}
+  # Replace feature name
+  WFS_CART_URL_final=${WFS_CART_URL_t1/feature_name/$feature}
+
   # Run script
   for ((retry = 1; retry <= max_retries; retry++)); do
       echo "Attempt $retry of $max_retries"
-      ogr2ogr -f "PostgreSQL" PG:"host=host.docker.internal user=citydb dbname=sudtirol_db password=citydb port=7777" -nln $table -a_srs EPSG:25832 WFS:$WFS_CART_URL $layer
+      ogr2ogr -f "PostgreSQL" PG:"host=host.docker.internal user=citydb dbname=sudtirol_db password=citydb port=7777" -nln $table -a_srs EPSG:25832 WFS:$WFS_CART_URL_final $layer
       if [ $? -eq 0 ]; then
             echo "Success: $pair"
             break
